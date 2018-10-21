892910dcdc057ef6abc9db018d830e5484e22b8d
heropoo <aiyouyou1000@163.com>
2018-10-21 19:53:28 +0800

    gitment

--- assets/lib/gitment/gitment.browser.js
+++ assets/lib/gitment/gitment.browser.js
@@ -4248,7 +4248,7 @@ var Gitment = function () {
 
       this.state.user.isLoggingIn = true;
       //http.post('https://gh-oauth.imsun.net', {
-      _utils.http.post('https://cors.wenjunjiang.win', {
+      _utils.http.post('https://cors.wenjunjiang.win/?remoteUrl=https://github.com/login/oauth/access_token', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
